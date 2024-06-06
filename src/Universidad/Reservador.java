b827437000e8779ba5a87814f0c22aadc41d61d9
Emmanuel Failla <emmafailla@gmail.com>
2024-06-06 10:23:24 -0300

    Reservadores Serializables

--- src/Universidad/Reservador.java
+++ src/Universidad/Reservador.java
@@ -1,8 +1,8 @@
 package Universidad;
-
+import java.io.Serializable;
 import java.time.LocalDate;
 
-abstract public class Reservador{
+abstract public class Reservador implements Serializable/*Para poder almacenar en archivos los objetos derivados de reservador*/ {
     public abstract String getCod();
     public float getCosto(){return 0;};
     @Override
